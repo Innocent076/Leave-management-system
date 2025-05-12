5e888f4b7bad73ad88027ec7a2949a902861d885
Innocent Sipho Mabasa <inocentsipho@gmail.com>
2025-05-12 14:49:13 +0200

    create user servlet edited

--- LeaveManagerWebApp/src/java/za/ac/tut/web/CreateUserServlet.java
+++ LeaveManagerWebApp/src/java/za/ac/tut/web/CreateUserServlet.java
@@ -8,20 +8,35 @@ package za.ac.tut.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import za.ac.tut.model.bl.UserFacadeLocal;
+import za.ac.tut.model.entity.User;
 
 /**
  *
  * @author SIBUSISO
  */
 public class CreateUserServlet extends HttpServlet {
-    @EJB 
+    @EJB UserFacadeLocal ufl;
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String uName = request.getParameter("name");
+        String pswd = request.getParameter("pswd");
+        String role = request.getParameter("role");
         
+        User user = new User();
+        user.setUserName(uName);
+        user.setPassword(pswd);
+        user.setRole(role);
+        
+        ufl.create(user);
+        
+        RequestDispatcher disp = request.getRequestDispatcher("");
+        disp.forward(request, response);
     }
 }
