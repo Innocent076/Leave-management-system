c796d0693bee10358dab831ee6f34061a517a05c
Innocent Sipho Mabasa <inocentsipho@gmail.com>
2025-05-12 14:38:15 +0200

    EJB created

---
+++ LeaveManagementEJBModule/src/java/za/ac/tut/model/entity/User.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.model.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author SIBUSISO
+ */
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Column(name = "Username")
+    private String userName;
+    @Column(name = "Password")
+    private String password;
+    @Column(name = "Role")
+    private String role;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    @Column(name = "Create date")
+    private Date createDate;
+
+    public User() {
+    }
+
+    public User(String id, String password, String role, Date createDate) {
+        this.userName = id;
+        this.password = password;
+        this.role = role;
+        this.createDate = createDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (userName != null ? userName.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the userName fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.userName == null && other.userName != null) || (this.userName != null && !this.userName.equals(other.userName))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "za.ac.tut.model.entity.User[ id=" + userName + " ]";
+    }
+    
+}
